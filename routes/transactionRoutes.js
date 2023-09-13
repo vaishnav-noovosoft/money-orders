820de09f058915fcb51f8dc2326f2bd11abbf03b
Pulkit <pulkit.dadheech@noovosoft.com>
2023-09-13 11:24:14 +0530

    Refactor transactionRoutes.js

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -14,8 +14,8 @@ const verifyType = async (req,res,next)=>{
         if(!user){
             return res.status(404).json({error: 'User not found'});
         }
-        const {type} = req.body.role;
-        if (type === 'user' || type === 'admin') {
+        const {role} = req.body.role;
+        if (role === 'user' || role === 'admin') {
             next();
         } else {
             return res.status(400).json({error: 'Required Type not Present'})
@@ -31,44 +31,49 @@ const verifyType = async (req,res,next)=>{
 
 }
 
-router.post('?type=deposit',verifyType, async (req, res) => {
-
+router.post('/',verifyType, async (req, res) => {
+    const type=req.body.query;
     try{
-        const {toUserName, amount}  = req.body;
-        const query = 'SELECT * FROM users WHERE username = $1';
-        const values = [toUserName];
-        const result = await db.query(query, values);
-        if(result.rows.length===0){
-            return res.status(404).json({ error: 'User not found' });
+        if(type === 'deposit'){
+            // depositUser(toUser,Amount);
+            const {toUserName, amount}  = req.body;
+            const query = 'SELECT * FROM users WHERE username = $1';
+            const values = [toUserName];
+            const result = await db.query(query, values);
+            if(result.rows.length===0){
+                return res.status(404).json({ error: 'User not found' });
+
+            }
+            const userId = result.rows.user_id;
+            await db.query(`INSERT into transactions(type,to_user,amount,date_created) values ($1,$2,$3,$4)`,['deposit',userId,amount,new Date().getDate()])
+            res.send('Money Deposited Successful');
 
         }
-        const userId = result.rows.user_id;
-        await db.query(`INSERT into transaction (type,to_user,amount,date_created) values ($1,$2,$3,$4)`,['deposit',userId,amount,new Date().getDate()])
-        res.send('Money Deposited Successful');
-
-    }catch (err){
-        res.status(500).send(err.message);
-    }
-})
-router.post('?type=withdraw',verifyType,async (req,res)=>{
-    try {
-        const {fromUserName, amount} = req.body;
-        const query = 'SELECT * FROM users where username=$1';
-        const values = [fromUserName];
-        const result = db.query(query, values);
-        if (result.rows.length === 0) {
-            return res.status(404).json({error: 'User not found'});
-        }money_orders_db
-        const userID = result.rows.user_id;
-        await db.query(`INSERT into transaction (type, from_user, amount, date_created)
+        else if(type === 'withdraw'){
+            // withdrawAmount(fromUser, amount);
+            const {fromUserName, amount} = req.body;
+            const query = 'SELECT * FROM users where username=$1';
+            const values = [fromUserName];
+            const result = db.query(query, values);
+            if (result.rows.length === 0) {
+                return res.status(404).json({error: 'User not found'});
+            }
+            const userID = result.rows.user_id;
+            await db.query(`INSERT into transactions (type, from_user, amount, date_created)
                         values ($1, $2, $3, $4)`, ['withdraw', userID, amount, new Date().getDate()]);
-        res.send('Money Withdraw Sucessfully');
+            res.send('Money Withdraw Sucessfully');
+        }
+        else if(type === 'transfer')
+        {
+            // transerAmount(fromUser, toUser, amount);
+            res.send('Transfer api remaining')
+        }
     }
+
     catch (err){
         res.status(500).send(err.message);
-
-
     }
 })
+
 module.exports = router;
 
